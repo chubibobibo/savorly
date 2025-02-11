da5e0ead7ce4ae0a46275486f5ecd4f3c8167e49
Alistair Abao <abaolester@gmail.com>
2025-02-12 00:42:35 +0100

    added custom sizes for buttons

--- client/src/pages/dashboardPages/LandingPage.tsx
+++ client/src/pages/dashboardPages/LandingPage.tsx
@@ -1,28 +1,30 @@
+import TransparentButton from "../../components/TransparentButton";
+
 function LandingPage() {
   return (
-    <section className=' flex flex-col items-center h-max-screen'>
+    <section className='flex flex-col items-center h-max-screen'>
       <img
         src='./registerBg.png'
         alt='background image'
-        className='w-screen h-screen fixed object-cover opacity-30'
+        className='w-screen h-screen fixed object-cover opacity-20'
       />
-      <section className='p-15 md:p-35'>
+      <section className='p-15 md:p-20'>
         <img
           src='./logo.png'
           alt='savorly logo'
           className='w-50 rounded-full relative md:w-90'
         />
       </section>
-      <section className='flex flex-col items-center gap-3 md:gap-6 md:text-lg'>
+      <section className='flex flex-col items-center gap-3 md:gap-6 md:text-lg z-10'>
         <section className='text-center pb-8'>
           <span className='font-rubik '>
             Create and save your own recipe by logging in
           </span>
         </section>
-        <section className='flex flex-col gap-3 md:flex-row md:gap-5'>
-          <button>Register</button>
-          <button>Login</button>
-          <button>Recipes</button>
+        <section className='flex flex-col gap-3 md:flex-row md:gap-5 z-10 items-center'>
+          <TransparentButton btnName={"Register"} size={"small"} />
+          <TransparentButton btnName={"Login"} size={"small"} />
+          <TransparentButton btnName={"Recipe Library"} size={"small"} />
         </section>
       </section>
     </section>
---
+++ client/src/utils/customBtnSize.ts
@@ -0,0 +1,3 @@
+export const customBtnSize = {
+  small: "w-[15rem]",
+};
