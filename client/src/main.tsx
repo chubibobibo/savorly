d8274e0c6aac7721c0e2f549878625886ae9a7e8
Alistair Abao <abaolester@gmail.com>
2025-02-13 23:46:55 +0100

    customized toast alert

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToastContainer position='top-center' />
+    <ToastContainer
+      position='top-center'
+      closeOnClick
+      transition={Zoom}
+      autoClose={8000}
+      hideProgressBar={true}
+      theme='colored'
+    />
     <App />
   </StrictMode>
 );
